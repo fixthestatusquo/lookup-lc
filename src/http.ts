e4e4ca0cd8668189e43f27c5ced3e68639955e9f
xavier <github@tttp.eu>
2026-03-05 05:47:15 +0800

    add a get route

--- src/http.ts
+++ src/http.ts
@@ -1,23 +1,26 @@
 import { FastifyPluginAsync } from "fastify";
 import lookup, { formatResult } from "./lookup";
 
-const httpRoutes: FastifyPluginAsync = async (fastify) => {
-  fastify.post("/lookup", async (request, reply) => {
-    try {
-      const email = (request.query as any)?.email;
-      if (!email) {
-        return reply.code(400).send({ error: "email query param required" });
-      }
-
-      const isSubscribed = await lookup(email);
-      return formatResult(isSubscribed);
-    } catch (error: any) {
-      console.error(error);
-      return reply
-        .code(500)
-        .send({ isError: true, error: error.toString(), found: false });
+const fetch = async (request, reply) => {
+  try {
+    const email = (request.query as any)?.email;
+    if (!email) {
+      return reply.code(400).send({ error: "email query param required" });
     }
-  });
+
+    const isSubscribed = await lookup(email);
+    return formatResult(isSubscribed);
+  } catch (error: any) {
+    console.error(error);
+    return reply
+      .code(500)
+      .send({ isError: true, error: error.toString(), found: false });
+  }
+};
+
+const httpRoutes: FastifyPluginAsync = async (fastify) => {
+  fastify.post("/lookup", async (request, reply) => fetch(request, reply));
+  fastify.get("/lookup", async (request, reply) => fetch(request, reply));
 };
 
 export default httpRoutes;
