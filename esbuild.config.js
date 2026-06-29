561fd0aa27870cc6b954139c105db5582cb9c0b6
1v4n4 <ivananl@yahoo.com>
2026-06-29 09:57:08 +0200

    Update readme

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,4 @@
-
-const esbuild = require("esbuild");
+import esbuild from "esbuild";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -7,9 +6,13 @@ esbuild.build({
   entryPoints: ["src/server.ts"],
   bundle: true,
   platform: "node",
-  target: "es2019",
+  format: "esm",
+  target: "es2020",
   outfile: "dist/server.js",
   sourcemap: !isProd,
   minify: isProd,
   external: ["better-sqlite3"],
+  banner: {
+    js: `import { createRequire } from "module"; const require = createRequire(import.meta.url);`,
+  },
 }).catch(() => process.exit(1));
